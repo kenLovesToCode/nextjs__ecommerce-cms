7c5629bbf809cea15d9cb5ea3fce620bf4b9db50
Jovanne Vicentino <jvicentino@contractors.stevensonsystems.com>
2023-07-29 22:15:45 +0800

    initialize and clerk authentication

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const SetupPage = () => {
+  return (
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default SetupPage;
