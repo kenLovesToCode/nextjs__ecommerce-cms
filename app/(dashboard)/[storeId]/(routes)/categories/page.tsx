2bfc4f364a92ef79a6c1b388971861e09ca75ec0
Jovanne Vicentino <lintuanken@gmail.com>
2023-08-04 17:56:18 +0800

    finish reusable data-table and initial categories

---
+++ app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -0,0 +1,33 @@
+import { format } from "date-fns";
+
+import prismadb from "@/lib/prismadb";
+import BillboardClient from "./components/client";
+import { BillboardColumn } from "./components/columns";
+
+const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedBillboards: BillboardColumn[] = billboards.map(
+    (billboard) => ({
+      ...billboard,
+      createdAt: format(billboard.createdAt, "MMMM dd, yyyy"),
+    })
+  );
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardClient data={formattedBillboards} />
+      </div>
+    </div>
+  );
+};
+
+export default BillboardsPage;
