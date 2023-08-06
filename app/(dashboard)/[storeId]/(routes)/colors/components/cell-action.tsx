6ae30e45c3246ab477d003d3df49717d847b8abe
Jovanne Vicentino <lintuanken@gmail.com>
2023-08-06 23:41:50 +0800

    Colors CRUD and UI

---
+++ app/(dashboard)/[storeId]/(routes)/colors/components/cell-action.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
+import { ColorColumn } from "./columns";
+import { Button } from "@/components/ui/button";
+import { toast } from "react-hot-toast";
+import AlertModal from "@/components/modals/alert-modal";
+
+interface CellActionProps {
+  data: ColorColumn;
+}
+
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success("Color id copied to clipboard");
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/colors/${data.id}`);
+      router.refresh();
+      toast.success("Color deleted.");
+    } catch (error) {
+      toast.error("Make sure you removed all products using this color first.");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button className="h-8 w-8 p-0" variant="ghost">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem
+            className="text-blue-600 cursor-pointer"
+            onClick={() => onCopy(data.id)}
+          >
+            <Copy className="mr-2 h-4 w-4" />
+            Copy Id
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="text-blue-600 cursor-pointer"
+            onClick={() => router.push(`/${params.storeId}/colors/${data.id}`)}
+          >
+            <Edit className="mr-2 h-4 w-4" />
+            Update
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="text-red-600 cursor-pointer"
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="mr-2 h-4 w-4" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
---
+++ app/(dashboard)/[storeId]/(routes)/colors/page.tsx
@@ -0,0 +1,33 @@
+import { format } from "date-fns";
+
+import prismadb from "@/lib/prismadb";
+import { ColorColumn } from "./components/columns";
+import { ColorsClient } from "./components/client";
+
+const ColorsPage = async ({ params }: { params: { storeId: string } }) => {
+  const colors = await prismadb.color.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedColors: ColorColumn[] = colors.map((color) => ({
+    id: color.id,
+    name: color.name,
+    value: color.value,
+    createdAt: format(color.createdAt, "MMMM dd, yyyy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ColorsClient data={formattedColors} />
+      </div>
+    </div>
+  );
+};
+
+export default ColorsPage;
